9bf093d760e3a6264a06904e90c4c2a3965c0abc
raj-humne <rajhumne3@gmail.com>
2025-11-30 19:41:55 +0530

    gsap animaiton scrolltrigger

--- lazarev.js
+++ lazarev.js
@@ -104,7 +104,21 @@ arrow.addEventListener("click", ()=>{
 arrow.children[1].classList.toggle("active")
 })
 })
-
+gsap.to(".btm8-p2 h4", {
+    x:150,
+    duration: 1,
+    stagger:{
+amount:-0.5
+    },
+    scrollTrigger:{
+        trigger:".btm8-p2 h4",
+        scroller:"body",
+        markers:true,
+        start: "top 95%",
+        end: "top -50%",
+        scrub:true
+    }
+})
 page3VideoAnim()
 // navAnimation()
 page2Anim()
