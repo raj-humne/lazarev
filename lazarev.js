b188aaaf6d2da91f911a7147e87646f95842be76
raj-humne <rajhumne3@gmail.com>
2025-12-02 21:01:17 +0530

    completed

--- lazarev.js
+++ lazarev.js
@@ -163,9 +163,32 @@ gsap.from(".btm8-p2 h4", {
     }
 })
 }
+function loadingAnimation(){
+    var tl = gsap.timeline()
+tl.from(".page1",{
+    opacity:0,
+    duration:0.3,
+    delay:0.2
+})
+tl.from(".page1",{
+    transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+    borderRadius:"100px",
+    duration:2,
+    ease:"expo.out"
+})
+tl.from("nav",{
+    opacity:0,
+    delay:-0.2
+})
+tl.from(".page1 h1, .page1 p, .page1 div",{
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+}
+loadingAnimation()
 locomotiveAnimation()
 page8Animations()
 page3VideoAnim()
-
-// navAnimation()
+ navAnimation()
 page2Anim()
